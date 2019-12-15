02cf0488441cdcea025b3b00c65916760af22099
Erik-Z <erikzhou7@gmail.com>
2019-12-14 23:55:46 -0500

    Final css tweaks

--- src/components/home_screen/HomeScreen.js
+++ src/components/home_screen/HomeScreen.js
@@ -29,7 +29,7 @@ class HomeScreen extends Component {
         return (
             <div className="z-depth-2" style={{
                 backgroundColor: "#55435f", paddingBottom: '70px', borderRadius: '0 0 10px 10px',
-                backgroundImage: 'linear-gradient(to bottom, #7F5A95, #473850)'
+                backgroundImage: 'linear-gradient(to bottom, #bdc3c7, #2c3e50)'
             }}>
                 <div className="dashboard container">
                     <div className="row">
